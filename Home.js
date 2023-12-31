9f5b07c8f1aad5d0ac4436c5f141c30bcf8896d6
annoui <ilyesannou77@gmail.com>
2023-12-31 20:46:14 +0300

    Home Page data ready

--- Home.js
+++ Home.js
@@ -5,12 +5,57 @@ import {
     Text,
     SafeAreaView,
     Button,
+    FlatList,
     TouchableOpacity
-  } from "react-native";
-
+} from "react-native";
+import axios from "axios";
 import { useNavigation } from "@react-navigation/native";
 
 const Home = () => {
+
+    const [expenses, setExpenses] = useState([]);
+    const [total, setTotal] = useState(0);
+
+    const fetchData = async () => {
+        try {
+            const result = await axios.get(
+                "http://localhost:3080/spending/"
+            );
+
+            //setData(extractData(result.data));
+            
+            setExpenses(/*extractExpensesData*/(result.data));
+            console.log("reesult")
+            console.log(result.data)
+            console.log("Expenses")
+            console.log(expenses)
+
+            let calculatedTotal = 0
+
+            result.data.forEach(expense => {
+                calculatedTotal+=expense.price;
+                
+            });
+            setTotal(calculatedTotal);
+            console.log("total "+total)
+            
+        } catch (error) {
+        console.warn(error);
+        }
+    };
+
+    /*const extractExpensesData = (data) => {
+        expensesList =[];
+        data.forEach((expense) => {
+            if(expense.description && expense.price){
+                expensesList.push({
+                    price: expense.price,
+                    description: expense.description
+                })
+            }
+        });
+        return expensesList;
+      };*/
 
     const navigation = useNavigation();
 
@@ -18,38 +63,95 @@ const Home = () => {
         navigation.navigate('LoginPage');
     };
 
-    
+    useEffect(() => {
+        fetchData();
+        const unsubscribe = navigation.addListener("focus", () => {
+            
+            return unsubscribe;
+        });
+        
+    }, [navigation]);
+
+    const renderExpenseItem = ({ item, index }) => (
+        <View key={index}>
+          <Text> {item.description} { item.price}$</Text>
+          
+        </View>
+    );
 
     useEffect(() => {
-        const unsubscribe = navigation.addListener("focus", () => {
-          return unsubscribe;
-        });
-    }, [navigation]);
+        console.log("Updated Expenses:", expenses);
+        console.log("Updated Total:", total);
+    }, [expenses, total]);
+
+    
 
     return (
 
         <SafeAreaView style={{flex: 1}} keyboardShouldPersistTaps='handled' >
             <View
                 style={{
-                    alignSelf:"center",
-                    marginTop:30,
+                    //alignSelf:"center",
+                    marginTop:10,
+                    paddingTop:20,
+                    paddingBottom:20,
+                    paddingLeft:10,
+                    borderRadius:20,
+                    backgroundColor: "white"
                 }}
             >
-                <Text>Hello, welcome to Thrift Saver !</Text>
+                <Text style={{
+                    fontWeight:"bold"
+                }}>Hello Username</Text>
             </View>
 
             <View>
                 <View
                     style={{
-                    }}   
-                >
-                <Text style = {{}}>My financials</Text>
+                    //alignSelf:"center",
+                    marginTop:10,
+                    paddingTop:20,
+                    paddingBottom:20,
+                    paddingLeft:10,
+                    borderRadius:20,
+                    backgroundColor: "white"
+                }}>
+                    <Text style = {{}}>Total expenses</Text>
+                    <Text style = {{
+                        alignSelf:"flex-end",
+                        fontWeight:"bold",
+                        color:"red"
+                    }}> { total } $</Text>
                 </View>
 
-                <View>
-                    <Text>Insights</Text> 
+                <View
+                style={{
+                    //alignSelf:"center",
+                    marginTop:10,
+                    paddingTop:20,
+                    paddingBottom:20,
+                    paddingLeft:10,
+                    borderRadius:20,
+                    backgroundColor: "white"
+                }}>
+                    <Text>Last Expenses</Text> 
+
+                    
+
+                    <FlatList
+                        data={expenses}
+                        renderItem={renderExpenseItem}
+                        keyExtractor={item => item.id}
+                        
+                    />
+
+
                 </View>
             </View>
+
+            <Button title ="test"  onPress={fetchData}>
+                <Text> Search </Text>
+            </Button>
 
             <Button title="Logout" onPress={handleLogout} /> 
         </SafeAreaView>
