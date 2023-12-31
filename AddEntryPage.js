2b5dcecc1f8791f7afe9e55145951a8aaf70d4dc
annoui <ilyesannou77@gmail.com>
2023-12-31 22:50:25 +0300

    add spending basic version

--- AddEntryPage.js
+++ AddEntryPage.js
@@ -1,22 +1,35 @@
 import { React, useState, useEffect } from "react";
-
+import axios from "axios";
 import {
     View,
     TextInput,
     StyleSheet,
-    Pressable,
+    Button,
     Text,
     TouchableOpacity,
     KeyboardAvoidingView,
     FlatList,
+    Platform, 
     Alert,
     SafeAreaView
   } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import DateTimePicker from "@react-native-community/datetimepicker"
 
 const AddEntryPage = () => {
+  const [amountInput, setAmountInput] = useState("");
+  const [descriptionInput, setDescriptionInput] = useState("");
+  const [date, setDate] = useState(new Date());
 
     const navigation = useNavigation();
+
+    const handleAmountInputChange = (text) => {
+      setAmountInput(text);
+    };
+
+    const handleDescriptionInputChange = (text) => {
+      setDescriptionInput(text);
+    };
 
     useEffect(() => {
         const unsubscribe = navigation.addListener("focus", () => {
@@ -24,12 +37,57 @@ const AddEntryPage = () => {
         });
     }, [navigation]);
 
+    const onChange = (event, date) => {
+      const currentDate = date || new Date();
+      setDate(currentDate);
+    };
+
+    const postSpending = async () => {
+      try {
+        const result = await axios.post(
+            "http://localhost:3080/spending/",
+            {
+              price: amountInput,
+              description: descriptionInput,
+              date:date
+            }
+        );
+        console.log('POST request successful:', result.data);
+      }
+      catch(error) {
+        console.error('Error making POST request:', error);
+      }
+    }
+
+  
+    
+
     return (
 
         <SafeAreaView style={{flex: 1}} keyboardShouldPersistTaps='handled' >
+          <TextInput
+            placeholder="Amount"
+            onChangeText={handleAmountInputChange}
+            value={amountInput.toString()}
+          />
+          <TextInput
+            placeholder="Description"
+            onChangeText={handleDescriptionInputChange}
+            value={descriptionInput.toString()}
+          />
           
-    
+            <DateTimePicker
+              testID="dateTimePicker"
+              value={date}
+              mode="date"
+              is24Hour={true}
+              display="default"
+              onChange={onChange}
+            />
+
+          <Button title="Add" onPress={postSpending} />
           
+        
         </SafeAreaView>
       );
 
